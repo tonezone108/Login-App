b6a83424e0715d0ceec70ab1af5101ffa49af934
tonezone108 <tonezone108@gmail.com>
2021-01-02 20:09:53 -0600

    updated one more time

--- login-app/Server/sql/connection.js
+++ login-app/Server/sql/connection.js
@@ -4,7 +4,7 @@ class Connection {
   constructor() {
     if (!this.pool) {
       console.log("creating mysql connection...");
-
+//did .env get ignored?
       this.pool = mysql.createPool({
         connectionLimit: 100,      
         host: process.env.REACT_APP_HOST,
