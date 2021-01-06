a9e32ef73c1ddbba095ae3adf84f2f12e185cd06
tonezone108 <tonezone108@gmail.com>
2021-01-05 21:44:37 -0600

    forget the .env for now

--- login-app/Server/sql/connection.js
+++ login-app/Server/sql/connection.js
@@ -7,10 +7,10 @@ class Connection {
 //did .env get ignored?
       this.pool = mysql.createPool({
         connectionLimit: 100,      
-        host: process.env.REACT_APP_HOST,
-        user: process.env.REACT_APP_USER,
-        password: process.env.REACT_APP_PASSWORD,
-        database: process.env.REACT_APP_DATABASE,
+        host: "den1.mysql6.gear.host",
+        user: "newappdatabase",
+        password: "Ab6wguJ~W_71",
+        database: "newappdatabase",
       });
 
 
