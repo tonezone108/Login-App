707e7d4fcad03b028858d536c8a3acc6a0309f8d
tonezone108 <tonezone108@gmail.com>
2021-01-05 19:29:03 -0600

    try again

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import {Provider } from "react-redux";
 import store from "./redux/store";
 import './App.css';
-//test commit
+//test commit again
 function App() {
   return (
     <Provider store={store}>
