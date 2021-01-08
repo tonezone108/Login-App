e60065637130c67a5cf72d2ada0a25cedfcebabc
tonezone108 <tonezone108@gmail.com>
2021-01-07 18:08:53 -0600

    editing the index.js for a test

--- login-app/Server/index.js
+++ login-app/Server/index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const usersRouter = require("./routers/users");
 const authRouter = require("./routers/auth");
-
 const { logger } = require("./middleware");
 const app = express();
 const port = process.env.PORT || 80;
 
-app.use(express.static("build"));
-//is this how we resolve the CORS error?
+// FIGURE OUT THE CORS ERROR
+// TypeError: NetworkError when attempting to fetch resource.
+
 
 app.use(function (req, res, next) {
-  // app.use(cors({ origin: ["https://localhost:4001"], credentials: true }));
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Content-Type", "application/json");
@@ -24,9 +22,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-app.use(express.json());
+
+app.use(bodyParser.json());
 app.use(logger);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
@@ -38,5 +35,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
 });
-
-//Howdy This is a test comment
