e734f851daeedd86efdd0c51e8613818fae39979
tonezone108 <tonezone108@gmail.com>
2021-01-07 18:58:09 -0600

    added .env

--- login-app/Server/sql/connection.js
+++ login-app/Server/sql/connection.js
@@ -7,18 +7,14 @@ class Connection {
 //did .env get ignored?
       this.pool = mysql.createPool({
         connectionLimit: 100,      
-        host: "den1.mysql6.gear.host",
-        user: "newappdatabase",
-        password: "Ab6wguJ~W_71",
-        database: "newappdatabase",
+      host: process.env.REACT_APP_HOST,
+      user: process.env.REACT_APP_USER,
+      password: process.env.REACT_APP_PASSWORD,
+      database: process.env.REACT_APP_DATABASE,
+
       });
 
 
-
-      // host: process.env.REACT_APP_HOST,
-      // user: process.env.REACT_APP_USER,
-      // password: process.env.REACT_APP_PASSWORD,
-      // database: process.env.REACT_APP_DATABASE,
 
 
 
